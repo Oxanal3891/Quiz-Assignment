002969de2713a94b3f7cb02c18350bc755827fed
Oxana <oxanal3891@gmail.com>
2023-12-05 01:27:31 +0000

    Added overall quiz and generate Qs functions, updated HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 var timer;
 var timerCount;
+var initials;
+var score;
 
-// Array of questions and answers
+// Array of questions and answer choices
 
 var questions = [
   {
     question: "To know the type of a JavaScript variable, we can use the _______ operator",
-    answers: {
+    choices: {
       1: 'typeof',
       2: 'Boolean',
       3: 'whattype'
@@ -15,7 +17,7 @@ var questions = [
   },
   {
     question: "Hoisting is the default behaviour of javascript where all declarations are moved _____ before code is executed",
-    answers: {
+    choices: {
       1: 'bottom',
       2: 'middle',
       3: 'top'
@@ -24,7 +26,7 @@ var questions = [
   },
   {
     question: "A callback is a function that will be executed _____ another function gets executed",
-    answers: {
+    choices: {
       1: 'after',
       2: 'before',
       3: 'when'
@@ -33,7 +35,7 @@ var questions = [
   },
   {
     question: "What is NaN property in JavaScript",
-    answers: {
+    choices: {
       1: 'Not-a-Number',
       2: 'Not-a-Null',
       3: 'Not-a-Node'
@@ -42,7 +44,7 @@ var questions = [
   },
   {
     question: "In Javascript, what would be the result of 3+2+”7″?",
-    answers: {
+    choices: {
       1: '12',
       2: '57',
       3: '5'
@@ -51,7 +53,7 @@ var questions = [
   },
   {
     question: "Global variables are the variables that are defined _______ functions.",
-    answers: {
+    choices: {
       1: 'inside of',
       2: 'outside of',
       3: 'with the'
@@ -60,7 +62,7 @@ var questions = [
   },
   {
     question: "Which company developed JavaScript?",
-    answers: {
+    choices: {
       1: 'Netscape',
       2: 'Microsoft',
       3: 'Apple'
@@ -68,3 +70,54 @@ var questions = [
     correctAnswer: '1'
   },
 ];
+
+// Function to run the quiz
+function runQuiz(questions, quizContainer, endScreenContainer, submitButton) {
+
+  function presentQuestions
+    (questions, quizContainer) {
+
+  }
+
+  function showScore(questions, quizContainer, endScreenContainer) {
+
+  }
+
+  function submitScore(score, endScreenContainer) { }
+
+  presentQuestions(questions, quizContainer);
+
+  showScore(questions, quizContainer, endScreenContainer);
+
+  submitButton.onclick = function () {
+    submitScore(score, endScreenContainer);
+  }
+}
+
+function presentQuestions
+  (questions, quizContainer) {
+  var questionTitle = [];
+  var choices;
+
+  for (var i = 0; i < questions.length; i++) {
+    choices = [];
+
+    for (number in questions[i].choices) {
+
+      choices.push(
+        '<label>'
+        + '<input type="radio" name="question' + i + '" value="' + number + '">'
+        + number + ': '
+        + questions[i].choices[number]
+        + '</label>'
+      );
+    }
+
+    questionTitle.push(
+      '<div class="question">' + questions[i].question + '</div>'
+      + '<div class="choices">' + choices.join('') + '</div>'
+    );
+  }
+
+  quizContainer.innerHTML = questionTitle.join('');
+}
