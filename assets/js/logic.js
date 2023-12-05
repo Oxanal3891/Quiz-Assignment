c34c3489e1b01da8063ebe5395a8167dd48e49ba
Oxana <oxanal3891@gmail.com>
2023-12-05 18:08:07 +0000

    Split into 2 js files, added startButton event, updated HTML with new variables

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,79 +1,12 @@
 var timer;
 var timerCount;
 var initials;
+let startButton = document.querySelector('.start');
+let startScreen = document.getElementById('start-screen');
 var quizContainer = document.getElementById('questions');
 var endScreenContainerContainer = document.getElementById('end-screen');
 var submitButton = document.getElementById('submit');
 
-// Array of questions and answer choices
-
-var questions = [
-  {
-    question: "To know the type of a JavaScript variable, we can use the _______ operator",
-    choices: {
-      1: 'typeof',
-      2: 'Boolean',
-      3: 'whattype'
-    },
-    correctAnswer: '1'
-  },
-  {
-    question: "Hoisting is the default behaviour of javascript where all declarations are moved _____ before code is executed",
-    choices: {
-      1: 'bottom',
-      2: 'middle',
-      3: 'top'
-    },
-    correctAnswer: '3'
-  },
-  {
-    question: "A callback is a function that will be executed _____ another function gets executed",
-    choices: {
-      1: 'after',
-      2: 'before',
-      3: 'when'
-    },
-    correctAnswer: 'a1'
-  },
-  {
-    question: "What is NaN property in JavaScript",
-    choices: {
-      1: 'Not-a-Number',
-      2: 'Not-a-Null',
-      3: 'Not-a-Node'
-    },
-    correctAnswer: '1'
-  },
-  {
-    question: "In Javascript, what would be the result of 3+2+”7″?",
-    choices: {
-      1: '12',
-      2: '57',
-      3: '5'
-    },
-    correctAnswer: '2'
-  },
-  {
-    question: "Global variables are the variables that are defined _______ functions.",
-    choices: {
-      1: 'inside of',
-      2: 'outside of',
-      3: 'with the'
-    },
-    correctAnswer: '2'
-  },
-  {
-    question: "Which company developed JavaScript?",
-    choices: {
-      1: 'Netscape',
-      2: 'Microsoft',
-      3: 'Apple'
-    },
-    correctAnswer: '1'
-  },
-];
-
-/*
 // Function to run the quiz
 function runQuiz(questions, quizContainer, endScreenContainer, submitButton) {
 
@@ -96,7 +29,15 @@ function runQuiz(questions, quizContainer, endScreenContainer, submitButton) {
     checkAnswers(score, quizContainer, endScreenContainer);
   }
 }
-*/
+
+//Change from the start screen to question on click of a start button and start the quiz
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  quizContainer.classList.remove("hide");
+  runQuiz()
+});
+
+//Present questions function
 function presentQuestions
   (questions, quizContainer) {
   var questionTitle = [];
@@ -152,4 +93,4 @@ function checkAnswers(questions, quizContainer, endScreenContainer) {
 
 
 }
-
+* /
